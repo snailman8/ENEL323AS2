c46f930bc5804a42293cc2926c02807b31303acb
Ben Snalam <snailman8@gmail.com>
2010-08-04 22:32:31 +1200

    added main

---
+++ main.c
@@ -0,0 +1,29 @@
+/**
+@File main.c
+@Author Ben Snalam and Kirstin Middelkoop
+@Date 22/7/2010
+@Brief test program for double_buffer
+*/
+
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "clock.h"
+#include "double_buffer.h"
+void main (void)
+{
+    uint8_t i = 0;
+    for (i =0; i<=20; i++)
+    {
+        uint8_t a;
+        printf("Enter a number into the buffer:\n");
+        
+        scanf ("%d",&a);
+        bf_wheel1_add(a);
+        test_print_rbuff();
+        test_print_wbuff();
+        printf("read buffer average: %i\n", bf_wheel1_average());
+    }
+    printf("end of test program\n");
+}
